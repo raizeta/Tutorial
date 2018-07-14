7f3db95699ae5fa081ffe7e14c513a6457faa770
raizeta <rai_zet@hotmail.com>
2018-07-14 11:57:44 +0700

    Migrasi Ke Macbook

--- Doctrine/generate.php
+++ Doctrine/generate.php
@@ -4,3 +4,4 @@
 3. php app/console doctrine:generate:entities AppBlogBundle/Entity/Post #ForSingleEntity -Post
 
 4.php app/console generate:doctrine:form AppBlogBundle:Post
+4.php app/console generate:doctrine:crud AppBlogBundle:Post
---
+++ Symfony/kembali-ke-page-sebelumnya.php
@@ -0,0 +1,10 @@
+<?php
+#Cara Pertama
+<a onclick="window.history.back()" class="btn btn-outline-secondary btn-block btn-sm" role="button" style="margin-bottom:10px;">
+    Kembali Ke List
+</a>
+
+#Cara Kedua
+<a onclick="window.history.go(-1); return false;" class="btn btn-outline-secondary btn-block btn-sm" role="button" style="margin-bottom:10px;">
+    Kembali Ke List
+</a>
