2e498020b62db1ec1d8eeaf059a571155a1c1c5d
Raizeta Corp <raizetacorp@gmail.com>
2022-02-26 13:50:25 +0700

    Pakai PAT

--- Apache/setting httpd.php
+++ Apache/setting httpd.php
@@ -1,5 +1,6 @@
 <?php
 //Ini adalah Komentar
+#Pakai Personala Access Token
 File:/private/etc/apache2/httpd.conf
 Jika Tidak Aktif,Aktifkan Terlebih Dahulu
 LoadModule rewrite_module libexec/apache2/mod_rewrite.so
